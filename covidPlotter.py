d33c08ab480d38c261fbfc534dc475f9ff2bcccb
Akeem Wells <awells@humite.local>
2020-04-12 16:10:13 -0400

    Added Population Data and Simple Analysis

--- covidPlotter.py
+++ covidPlotter.py
@@ -32,10 +32,10 @@ def plot_country_confirmed_cases_index(dataframe,country, expGraph = False):
     dates = dataframe.columns[4:]
     cases_per_day = dataframe.values[0][4:]
     res = next((i for i, j in enumerate(cases_per_day) if j), None) # Finds Where the first day is non-zero
-    cases = cases_per_day[res:]
-    cases = numpy.insert(cases,0,0)
+    #cases = cases_per_day[res:]
+    #cases = numpy.insert(cases,0,0)
+    cases = [i for i in cases_per_day[res:] if i >= 100]
     days_since_outbreak = list(range(0, len(cases)))
-    
     plt.figure()
     plt.plot(days_since_outbreak,cases, label="{}".format(country))
     
@@ -45,13 +45,21 @@ def plot_country_confirmed_cases_index(dataframe,country, expGraph = False):
             n = n + 1
         n = n - 1
         plt.plot( numpy.linspace(0,n,100),numpy.exp(numpy.linspace(0,n,100)), 'k--',label="exp(x)")
-    plt.xlabel("Days Since 01/22/2020")
+    plt.xlabel("Days Since 100th Confirmed Case")
     plt.ylabel('{} Confirmed Cases'.format(country))
     plt.legend()
     
 def plot_bar(array,country,ylabel=''): 
     plt.figure()
+    array = [i for i in array if i >= 100]
     plt.bar(list(range(len(array))), array,label=country)
-    plt.xlabel("Days Since 01/22/2020")
+    plt.xlabel("Days Since 100th Confirmed Case")
+    plt.ylabel(ylabel)
+    plt.legend()
+    
+def plot_bar_active(array,country,ylabel=''): 
+    plt.figure()
+    plt.bar(list(range(len(array))), array,label=country)
+    plt.xlabel("Days Since 100th Confirmed Case")
     plt.ylabel(ylabel)
     plt.legend()
--- webscraper.py
+++ webscraper.py
@@ -23,3 +23,32 @@ def webscrape(datapath,url,ext, verbose=False):
 
     print("Data is Stored in: {}".format(datapath + "/"))
 
+def webscrape_population_2020():
+    from bs4 import BeautifulSoup
+    import requests
+    import pandas
+    url = "https://www.worldometers.info/world-population/population-by-country/"
+    print("Fetching Data From: {}".format(url))
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    headers = ["country", "population", "yearlyChange","netChange","denstity(P/Km2)","landArea(km2)", "migrants", "fertRate","medAge", "urbanPop","worldShare"]
+    table = soup.find('table')
+    table_rows = table.find_all('tr')
+    frame = []
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text for i in td]
+        if len(row) == 0:
+            continue
+        ## Clean Data
+        entry = row[1:]
+        entry[1] = entry[1].replace(',', '')
+        entry[3] = entry[3].replace(',', '')
+        entry[5] = entry[5].replace(',', '')
+        entry[6] = entry[6].replace(',', '')
+
+        frame.append(entry)
+
+    df = pandas.DataFrame(frame, columns = headers) 
+    return df 
